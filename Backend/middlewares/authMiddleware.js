d025316c1b15911819832132ddd4bbf519420820
sahilnikalje <sahilnik88@gmail.com>
2026-02-18 19:30:50 +0530

    authMiddleware added

---
+++ Backend/middlewares/authMiddleware.js
@@ -0,0 +1,31 @@
+const jwt=require('jsonwebtoken')
+const User=require('../models/userSchema')
+
+const protect=async(req,res,next)=>{
+    try{
+        const token=req.headers.authorization.split(" ")[1]
+        if(!token){
+            return res.status(401).json({success:false, message:"No token"})
+        }
+
+        const decoded=jwt.verify(token, process.env.JWT_SECRET)
+        req.user=await User.findById(decoded.id).select('-password')
+        if(!req.user){
+            return res.status(401).json({success:false, message:"User not found"})
+        }
+
+        next()
+    }
+    catch(err){
+        res.status(401).json({success:false, message:err.message})
+    }
+}
+
+const adminOnly=(req,res,next)=>{
+    if(req.user.role!=='admin'){
+        return res.status(403).json({success:false, message:"Not authorized"})
+    }
+    next()
+}
+
+module.exports={protect, adminOnly}
