ea8d35ca6103336ba3913a761f4fa6c8cff2c80a
sahilnikalje <sahilnik88@gmail.com>
2026-02-20 10:25:22 +0530

    minor change for deployement

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ const PORT=process.env.PORT
 
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(cors({origin:'http://localhost:5173', credentials:true}))
+app.use(cors({origin:process.env.VITE_URI, credentials:true}))
 
 const limiter=rateLimit({windowMs:15*60*1000, max:100})
 
